96df79d45b27b8e5c31061538d3e710bbb16e5c9
root <shamim.ice.ewu@gmail.com>
2024-01-20 10:49:24 +0000

    Fixed Migration

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
